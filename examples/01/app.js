1e5bcd76e80678af166a5382a7a07f85ed4ec030
Sergey Burmisov <skypath@gmail.com>
2015-10-15 15:51:46 +0300

    upgrade to react 0.14

--- examples/01/app.js
+++ examples/01/app.js
@@ -2,7 +2,8 @@ require('./index.html');
 require('bootstrap/dist/css/bootstrap.css');
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 const approotNode = document.getElementById('approot');
 
-React.render(<h1>Hello</h1>, approotNode);
+ReactDOM.render(<h1>Hello</h1>, approotNode);
